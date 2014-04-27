d03f44b989459681f4e07866d1d4e34428d871e1
Jeremy Banks <j@jeremybanks.ca>
2014-04-27 13:45:32 -0400

    Adds ChatExchange as a dependency.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='stack-suggestions-bot',
+    version='0.0.0dev1',
     url='https://github.com/jeremybanks/stack-suggestions-bot',
     author='Jeremy Banks',
     author_email='j@jeremybanks.ca',
@@ -22,6 +23,11 @@ setuptools.setup(
         'pprintpp==0.2.1',
         'pytest==2.5.2',
         'pytest-capturelog==0.7',
-        'requests==2.2.1'
+        'requests==2.2.1',
+        'ChatExchange==0.0.0dev1'
+    ],
+    dependency_links=[
+        ("git+ssh://git@github.com/jeremybanks/ChatExchange.git"
+         "@dd4ed4f4e22f6f668d1c60c4f7fa09cf43f2e290#egg=ChatExchange-0.0.0dev1")
     ]
 )
